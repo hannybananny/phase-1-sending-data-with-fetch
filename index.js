8fddc644925027f7fa5b2a34e67fd5ad1658f899
Hannah <hannahmgrout@gmail.com>
2022-04-17 12:28:55 -0700

    commit tree 7858e0c8cd98fd406de727f51b5a41c3d5677ad0

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(userName, userEmail){
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+    body: JSON.stringify({
+        "name": userName, 
+        "email": userEmail
+    })
+    }
+    return fetch('http://localhost:3000/users', configObj)
+    .then(res => res.json())
+    .then(data => handleID(data.id))
+    .catch(error => {
+        let message = `${error}`
+        let p = document.createElement('p')
+        p.innerHTML = message
+        document.body.appendChild(p)
+        })
+}
+
+function handleID(newID){
+   let p = document.createElement('p')
+    p.textContent = `${newID}`
+    document.body.appendChild(p)
+}
+
+submitData()
